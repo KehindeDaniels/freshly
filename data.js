bd275d72cd42f3de207a58a45b86a2aa127c78e5
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-07 00:14:49 +0100

    moved asset to public

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const products = [
       "Fresh lettuce leaves, perfect for salads and garnishes, packed with nutrients.",
     category: "Vegetables",
     price: 900,
-    image: "./src/assets/lettuce.png",
+    image: "./public/assets/lettuce.png",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const products = [
       "Ripe, juicy tomatoes ideal for cooking or fresh eating, rich in vitamins and antioxidants.",
     category: "Vegetables",
     price: 1200,
-    image: "./src/assets/tomatoes.png",
+    image: "./public/assets/tomatoes.png",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const products = [
       "Versatile onions, essential for flavor in a variety of dishes, available per kilogram.",
     category: "Vegetables",
     price: 900,
-    image: "./src/assets/onions.png",
+    image: "./public/assets/onions.png",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const products = [
       "Bright and zesty lemons, perfect for enhancing drinks and dishes with a citrusy flavor.",
     category: "Fruits",
     price: 400,
-    image: "./src/assets/lemon.png",
+    image: "./public/assets/lemon.png",
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ const products = [
       "Spicy Scotch Bonnet peppers to add heat and flavor to your meals, sold per kilogram.",
     category: "Vegetables",
     price: 600,
-    image: "./src/assets/scotch.png",
+    image: "./public/assets/scotch.png",
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ const products = [
       "Smooth and versatile eggplants, great for grilling, baking, or frying.",
     category: "Vegetables",
     price: 900,
-    image: "./src/assets/Egg-plant.png",
+    image: "./public/assets/Egg-plant.png",
   },
   {
     id: 7,
@@ -60,7 +60,7 @@ const products = [
       "Sweet and creamy bananas, perfect for snacking or baking, available per bunch.",
     category: "Fruits",
     price: 1000,
-    image: "./src/assets/Banana.png",
+    image: "./public/assets/Banana.png",
   },
   {
     id: 8,
