0db3684d44b44a74a92d563fa28c563f472ac67a
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-07 00:26:27 +0100

    adjusted file path again

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const products = [
       "Fresh lettuce leaves, perfect for salads and garnishes, packed with nutrients.",
     category: "Vegetables",
     price: 900,
-    image: "./public/assets/lettuce.png",
+    image: "/assets/lettuce.png",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const products = [
       "Ripe, juicy tomatoes ideal for cooking or fresh eating, rich in vitamins and antioxidants.",
     category: "Vegetables",
     price: 1200,
-    image: "./public/assets/tomatoes.png",
+    image: "/assets/tomatoes.png",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const products = [
       "Versatile onions, essential for flavor in a variety of dishes, available per kilogram.",
     category: "Vegetables",
     price: 900,
-    image: "./public/assets/onions.png",
+    image: "/assets/onions.png",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const products = [
       "Bright and zesty lemons, perfect for enhancing drinks and dishes with a citrusy flavor.",
     category: "Fruits",
     price: 400,
-    image: "./public/assets/lemon.png",
+    image: "/assets/lemon.png",
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ const products = [
       "Spicy Scotch Bonnet peppers to add heat and flavor to your meals, sold per kilogram.",
     category: "Vegetables",
     price: 600,
-    image: "./public/assets/scotch.png",
+    image: "/assets/scotch.png",
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ const products = [
       "Smooth and versatile eggplants, great for grilling, baking, or frying.",
     category: "Vegetables",
     price: 900,
-    image: "./public/assets/Egg-plant.png",
+    image: "/assets/Egg-plant.png",
   },
   {
     id: 7,
@@ -60,7 +60,7 @@ const products = [
       "Sweet and creamy bananas, perfect for snacking or baking, available per bunch.",
     category: "Fruits",
     price: 1000,
-    image: "./public/assets/Banana.png",
+    image: "/assets/Banana.png",
   },
   {
     id: 8,
@@ -69,7 +69,7 @@ const products = [
       "Fresh pineapple, juicy and packed with vitamins, ideal for a healthy snack.",
     category: "Fruits",
     price: 1900,
-    image: "./src/assets/Pineapple.png",
+    image: "/assets/Pineapple.png",
   },
 ];
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "/assets/logo.png";
 import { FaSearch, FaShoppingCart } from "react-icons/fa";
 
 const NavBar = () => {
