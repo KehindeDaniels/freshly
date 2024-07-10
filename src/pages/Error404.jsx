c82849f7ca42f56570cfaca59dc9c63c3ad212c9
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-10 16:43:57 +0100

    added skelonton

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -22,12 +22,12 @@ const Error404 = () => {
     <div className="bg-black text-white h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl mb-4 font-bold">Dear Mentor</h1>
       <h1 className="text-3xl font-bold text-center">
-        {`Welcome to the darkweb, you have ${count} seconds to leave or else you will be initiated`}
+        {`Welcome to the darkweb 🩻, you have ${count} seconds to leave or else you will be initiated`}
       </h1>
       <p className="text-2xl mt-5">{count}</p>
       {count === 0 && (
         <div className="flex flex-col justify-center items-center">
-          <p className="text-xl mt-5">Why didn't you leave?</p>
+          <p className="text-xl mt-5">Why didn't you leave ?</p>
           {showButton && (
             <Link to={".."}>
               <button className="mt-5 px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition duration-100">
